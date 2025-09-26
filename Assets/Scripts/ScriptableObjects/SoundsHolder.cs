c019687efa10b1929242ba31c89128eb677f9511
PrabuRaz <praburaj1730@gmail.com>
2025-09-26 11:57:11 +0100

    GameSounds scriptable added

--- Assets/Scripts/ScriptableObjects/SoundsHolder.cs
+++ Assets/Scripts/ScriptableObjects/SoundsHolder.cs
@@ -10,6 +10,8 @@ public class SoundData
     public AudioClip AudioClip;
     public nlEnum.GameSounds gameSounds;
 }
+
+[CreateAssetMenu(fileName = "GameAudios", menuName = "Scriptable Objects/GameAudios")]
 public class SoundsHolder : ScriptableObject
 {
     [SerializeField] List<SoundData> soundDatas = new List<SoundData>();    
