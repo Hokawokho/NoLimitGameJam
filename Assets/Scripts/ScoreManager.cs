840f27426bdb0e0984b4f91c2299c06b1968b64f
Jose Pablo Gonzalez  Poblette <pablogpp88@gmail.com>
2025-09-26 18:25:03 +0100

    Food quality Bar

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -1,21 +1,38 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     [Header("References")]
     
-    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private Slider foodQualityBar;
+    [SerializeField] private Image foodQualityBarFill;
+    [SerializeField] private RectTransform expectedScoreLabel;
     
     [Header("Settings")]
     
+    [SerializeField] private Color goodColor;
+    [SerializeField] private Color badColor;
+    
     [SerializeField] private int mealScore;
     [SerializeField] private int expectedScore;
+    [SerializeField] private int foodQualityBarSpeed;
+    
+    private const float MIN_POSITION = -196.38f;
+    private const float MAX_POSITION = 196.38f;
+
+    private void Start()
+    {
+        UpdateExpectedScoreLabelPosition();
+    }
 
     private void Update()
     {
-        scoreText.text = mealScore.ToString();
+        foodQualityBar.value = Mathf.Lerp(foodQualityBar.value, mealScore, foodQualityBarSpeed * Time.deltaTime);
+
+        foodQualityBarFill.color = mealScore < expectedScore ? badColor : goodColor;
     }
 
     public void AddMealScore(int newScore)
@@ -26,6 +43,16 @@ public class ScoreManager : MonoBehaviour
         {
             mealScore = 0;
         }
+    }
+    
+    private void UpdateExpectedScoreLabelPosition()
+    {
+        float normalizedScore = expectedScore / 100f; // Convert to 0-1 range
+        float targetPosition = Mathf.Lerp(MIN_POSITION, MAX_POSITION, normalizedScore);
+        
+        // Update the X position of the label
+        Vector3 currentPosition = expectedScoreLabel.anchoredPosition;
+        expectedScoreLabel.anchoredPosition = new Vector2(targetPosition, currentPosition.y);
     }
 
     public int GetMealScore()
