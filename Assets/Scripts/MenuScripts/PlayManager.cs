efb36387e043016b0b5f4227e57df46e7a22d66b
Hokawokho <jordipeiropuig@gmail.com>
2025-10-02 16:43:48 +0100

    Logica final

--- Assets/Scripts/MenuScripts/PlayManager.cs
+++ Assets/Scripts/MenuScripts/PlayManager.cs
@@ -4,13 +4,13 @@ using UnityEngine.SceneManagement;
 public class PlayManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void playGame()
+    public void playGame()
     {
         Debug.Log("Start Game");
         SceneManager.LoadScene(1, LoadSceneMode.Single);
     }
 
-    void exitGame()
+     public void exitGame()
     {
         Debug.Log("Exit Level");
         Application.Quit();
