dab2c0250669c84927bd245d1b1d255914ac8744
jp-12305 <68533002+PrabuRajM10@users.noreply.github.com>
2025-10-02 16:43:19 +0100

    Game end screen

---
+++ Assets/Scripts/Gameplay/GameEndUi.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Gameplay
+{
+    public class GameEndUi : MonoBehaviour
+    {
+        [SerializeField] CanvasGroup   canvasGroup;
+        [SerializeField] private Button restartButton;
+        [SerializeField] private Button quitButton;
+        [SerializeField] private Image bg;
+
+        private void OnValidate()
+        {
+            if(canvasGroup == null)canvasGroup = GetComponent<CanvasGroup>();
+        }
+
+        private void OnEnable()
+        {
+            restartButton.onClick.AddListener(OnClickRestartButton);
+            quitButton.onClick.AddListener(OnClickQuitButton);
+            RatingsManager.GameEnd += OnGameEnd;
+            bg.raycastTarget = false;
+            canvasGroup.alpha = 0;
+        }
+
+        private void OnDisable()
+        {
+            restartButton.onClick.RemoveListener(OnClickRestartButton);
+            quitButton.onClick.RemoveListener(OnClickQuitButton);
+            RatingsManager.GameEnd -= OnGameEnd;
+        }
+
+        private void OnGameEnd()
+        {
+            bg.raycastTarget = true;
+            canvasGroup.DOFade(1, 0.5f);
+        }
+
+        private void OnClickQuitButton()
+        {
+            #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+            #else
+            Application.Quit();
+            #endif
+        }
+
+        private void OnClickRestartButton()
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
